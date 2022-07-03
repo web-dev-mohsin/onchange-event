f0bde0a7d2163dd0def20f5ad85d0907ac0f8499
mohsin <mdmohsineng@gmail.com>
2022-07-04 00:46:30 +0600

    data read from checkbox

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ function myfunc(e){
     console.log(e.target.value);
 
 }
+
+const programs = document.querySelectorAll("input[name=program]");
+
+Array.from(programs).map((program) => {
+    program.addEventListener("change", programHandaler)
+})
+
+function programHandaler(e){
+    if(e.target.checked){
+        console.log(e.target.value);
+    }else{
+        console.log(e.target.value +" is unchecked")
+    }
+}
